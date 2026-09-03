726437748b3c50d4dd6ae5fe7b59d9328dd04069
farahafrinm <farahafrin.m@gmail.com>
2026-09-03 13:37:32 +0530

    Update server.js

--- Hillora-app/server.js
+++ Hillora-app/server.js
@@ -106,6 +106,7 @@ app.post('/api/login', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
